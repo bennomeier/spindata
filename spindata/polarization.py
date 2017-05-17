b0710eccff95d29e3b417fca8ce1ac6970b0cdca
bennomeier <meier.benno@gmail.com>
2017-05-17 22:01:47 +0100

    Fix Python3 incompatibility in polarization.py

--- spindata/polarization.py
+++ spindata/polarization.py
@@ -4,7 +4,7 @@ from scipy.constants import k, hbar
 from spindata.gamma import gamma
 import numpy as np
 
-#  P = N \left ( \frac{2}{1 + \exp ( - \Delta E / k_B T)} - 1 \right )
+#  P = N \left ( \frac{2}{1 + \exp ( - \Delta E / k_B T)} - 1 \right ) = Tanh( \Delta e / 2 k_B T)
 
 def polarization(nucleus, B, T):
     """This function returns the thermal polarization at a given temperature.
@@ -16,13 +16,12 @@ def polarization(nucleus, B, T):
     """
     g = gamma(nucleus)
     DE = hbar*g*B
-    polarization = 2/(1 + np.exp(- DE/(k*T))) -1
-
+    
+    polarization = np.tanh(DE/(2*k*T))
     return polarization
 
 if __name__ == "__main__":
-    print "E polarization at 3.35 Tesla, 1 K: ", polarization("E", 3.35, 1)
-    print "13C polarization at 3.35 Tesla, 1.5K: ", polarization("13C", 3.35, 1)
-    print "13C polarization at 9 Tesla, 300K: ", polarization("13C", 9, 300)
-    print "1H polarization at 11.7 Tesla, 300K: ", polarization("1H", 11.7, 300)
-    print "Ratio between those: ", polarization("E", 3.35, 1.5)/polarization("13C", 9, 300)
+    print("E polarization at 3.35 Tesla, 1 K: {}".format(polarization("E", 3.35, 1)))
+    print("13C polarization at 3.35 Tesla, 1.5K: {}".format(polarization("13C", 3.35, 1)))
+    print("13C polarization at 9 Tesla, 300K: {}".format(polarization("13C", 9, 300)))
+    print("Ratio between E and 13C those: {}".format(polarization("E", 3.35, 1.5)/polarization("13C", 9, 300)))
